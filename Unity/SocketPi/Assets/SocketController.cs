dd1856f329c28a3d3df6cd8cf6937b1b367a9c5b
zackm0571 <zack@zackmatthews.com>
2018-03-07 15:49:50 -0500

    Add Unity project

---
+++ Unity/SocketPi/Assets/SocketController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Quobject.SocketIoClientDotNet.Client;
+using UnityEngine;
+
+public class SocketController : MonoBehaviour {
+	public GameObject obj;
+	protected string sockPos;
+	// Use this for initialization
+	void Start () {
+		var socket = IO.Socket ("http://192.168.2.3:3000");
+		socket.On (Socket.EVENT_CONNECT, () => {
+			socket.Emit("pos");
+		});
+		socket.On("pos", (data) =>
+			{
+				Debug.Log(data);
+				//socket.Disconnect();
+
+				sockPos = data.ToString();
+			});
+
+//		socket.On("pos", (data) =>
+//			{
+//				int posInfo;// = Int32.parse(data.ToString());
+//				Vector3 newPos = new Vector3(posInfo, posInfo, posInfo);
+//				obj.transform = Vector3.Lerp(obj.transform.position, newPos);
+//			});
+	}
+	// Update is called once per frame
+	void Update () {
+		if (sockPos != null) {
+			char[] delimiter = { ',' };
+			string[] vectorString = sockPos.Split(delimiter, 3);
+			Vector3 newPos = new Vector3 (float.Parse (vectorString[0]), float.Parse (vectorString[1]), float.Parse (vectorString[2]));
+			obj.transform.position = Vector3.Lerp (obj.transform.position, obj.transform.position + newPos, 0.5f);
+			sockPos = null;
+		}
+	}
+}
